7d83e28c4c69e7aa61eebc1093c1ca9daf72f980
yeosh <yeosh@DESKTOP-UFRPLR9.fios-router.home>
2020-10-31 11:28:54 -0400

    made a 3rd change

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -8,7 +8,9 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
-		
+		System.out.println("Week 9 change");
+		System.out.println("Week 9 second change");
+
 
 	}
 	
