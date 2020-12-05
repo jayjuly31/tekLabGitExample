f56f8f0490ce2b83c70337bab93181f1834a92b9
Dee <Dee@VICMONLT>
2020-12-05 16:13:12 -0500

    Added a good method in HelloTeklab

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -3,13 +3,20 @@ package example;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 
+
+
+
 public class HelloTekLab {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
 		
-
+		System.out.println("This is Peter's change");
+		
+			
+			
+		}
 	}
 	
 	@BeforeMethod
@@ -17,6 +24,13 @@ public class HelloTekLab {
 		String driverpath = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverpath);
 	}
+	
+	public void goodMethod() {
+		System.out.prinitln("this is a new method");
+		
+	}
+		
+	}
 
-}
+
 
