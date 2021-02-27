f5e1852c7751c743e09720f848439d627200b3aa
jayC <jaywaynesky@gmail.com>
2021-02-27 12:37:30 -0500

    remove greeting code and created new feature

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -7,11 +7,7 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
-
-		System.out.println("JayC");
-		System.out.println("Hello");
-		System.out.println("HudaS");
-		System.out.println("third line");
+		System.out.println("created a new feature");
 
 	}
 
