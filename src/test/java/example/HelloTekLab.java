715f5c1b988fd8b1cc2638283d65ed3168ede3ef
jayC <jaywaynesky@gmail.com>
2021-02-27 01:02:13 -0500

    Refresh file

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -1,6 +1,5 @@
 package example;
 
-
 import org.testng.annotations.BeforeMethod;
 
 public class HelloTekLab {
@@ -8,23 +7,17 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
-		System.out.println("This is my third commit today");
-		
-	    System.out.println("this is darcey's change");
-	    
-	
 	}
-	
+
 	@BeforeMethod
 	public void setUp() {
 		String driverpath = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverpath);
 	}
-    public void goodMethod() {
-    System.out.println("this is a new method");	
-    	
-    	
-    }
+
+	public void goodMethod() {
+		System.out.println("this is a new method");
+
+	}
 
 }
-
