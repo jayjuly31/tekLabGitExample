3ae5f6c02237953b80dd88a29ce182652e0564a7
kamran <kamran@DESKTOP-TUN0UMG>
2021-03-04 16:54:54 -0800

    add Hello

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -8,6 +8,7 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 		System.out.println("Hello TekLab Students");
 		System.out.println("Hi jay c");
+		System.out.println("Hello");
 		
 	}
 
